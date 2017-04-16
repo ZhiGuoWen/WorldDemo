b7a8bcabe64f1f88f8119a20cb76d122d40d76d6
wenzhiguo <wenzhiguo0414@foxmail.com>
2017-04-16 18:37:53 +0800

    oom解决利用别人的依赖

--- app/src/main/java/com/wenzhiguo/worlddemo/MainActivity.java
+++ app/src/main/java/com/wenzhiguo/worlddemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.wenzhiguo.worlddemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.wenzhiguo.worlddemo.view.ImageSurfaceView;
+
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
+            /*自定义
             Custom mCustom = (Custom) findViewById(R.id.custom);
             InputStream open = getAssets().open("world.jpg");
-            mCustom.setInput(open);
+            mCustom.setInput(open);*/
+            ImageSurfaceView viewById = (ImageSurfaceView) findViewById(R.id.image);
+            InputStream open = getAssets().open("world.jpg");
+            viewById.setInputStream(open);
         } catch (Exception e) {
             e.printStackTrace();
         }
